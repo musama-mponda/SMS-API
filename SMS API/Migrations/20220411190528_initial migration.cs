bdb07812735ed892b1506dac8c112371e35fea9b
dalitso <tembodalitso22@gmail.com>
2022-04-12 08:26:22 +0200

    new changes

---
+++ SMS API/Migrations/20220411190528_initial migration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS_API.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Message = table.Column<string>(type: "TEXT", nullable: false),
+                    Sender = table.Column<string>(type: "TEXT", nullable: false),
+                    Receiver = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Delivered = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
---
+++ SMS API/Migrations/20220412054202_changed column names.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS_API.Migrations
+{
+    public partial class changedcolumnnames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sender",
+                table: "Messages",
+                newName: "Text");
+
+            migrationBuilder.RenameColumn(
+                name: "Receiver",
+                table: "Messages",
+                newName: "OriginatorId");
+
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "Messages",
+                newName: "Msisdn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Messages",
+                newName: "Sender");
+
+            migrationBuilder.RenameColumn(
+                name: "OriginatorId",
+                table: "Messages",
+                newName: "Receiver");
+
+            migrationBuilder.RenameColumn(
+                name: "Msisdn",
+                table: "Messages",
+                newName: "Message");
+        }
+    }
+}
